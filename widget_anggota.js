977626cf9839dffed110d1b20bc93ce420f9446b
dinocerata <dino.cerata@gmail.com>
2016-05-19 18:12:28 +0700

    Create widget_anggota.js

---
+++ widget_anggota.js
@@ -0,0 +1,2 @@
+document.write("<SCRIPT language='JavaScript' SRC='http://m-edukasi.googlecode.com/files/style.js'></SCRIPT>");
+document.write("<SCRIPT language='JavaScript' SRC='http://formulasi.googlecode.com/files/kode_formulasi.js'></SCRIPT>");
